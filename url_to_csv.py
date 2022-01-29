a8f2a627c8ef77ba26a1d3bf1b78b228a9eaf889
Maria Briceno <mafer09@users.noreply.github.com>
2022-01-29 13:58:10 -0600

    clean version

--- url_to_csv.py
+++ url_to_csv.py
@@ -3,6 +3,7 @@ import urllib.request
 import re
 import numpy as np
 
+
 '''
     input: list of list
     output: flattened list
@@ -11,6 +12,10 @@ import numpy as np
 def flatten(data):
     return [item for sublist in data for item in sublist]
 
+'''
+    input: list of stations
+    output: list of urls pointing to those stations
+'''
 def get_url_list(station_list):
     url_list = []
     source_url = "https://www.ndbc.noaa.gov/data/realtime2/"
@@ -21,19 +26,16 @@ def get_url_list(station_list):
 
 #list of federal aviation administration stations
 faas_station_list = ['KATP','KBBF','KBQX','KCMB','KCRH','KCVW','KDLP','KEHC','KEIR','KEMK','KGBK','KGHB','KGRY','KGUL','KGVX','KHHV','KHQI','KIKT','KIPN','KMDJ','KMIS','KMIU','KMYT','KMZG','KOPM','KSCF','KSPR','KSQE','KSTZ','KVAF','KVBS','KVKY','KVNP','KVOA','KVQT','KXIH','KXPY']
-#++".txt"
+
 faas_url = get_url_list(faas_station_list)
 header = ["year", "month", "day", "hour", "minute", "WDIR_wind_direction","WSPD_wind_speed","GST_gust_speed","WVHT_wave_height","DPD_dominant_wave_period","APD_average_wave_period","MWD_wave_degree","PRES_sea_level_pressure","ATMP_air_temperature","WTMP_sea_surface_temperature","DEWP_dewpoint_temperature","VIS_station_visibility","PTDY_pressure_tendency","TIDE_water_level","station_name"]
-
-# file = urllib.request.urlopen("https://www.ndbc.noaa.gov/data/realtime2/KCVW.txt")
-# print(type(file))
 
 combined_files = []
 for url in faas_url:
     #retrieve station name from url
     pattern = '2/(.*?)\.'
     station_name = re.search(pattern, url).group(1)
-    print(station_name)
+    #print(station_name)
     #get data
     try:
         file = urllib.request.urlopen(url)
@@ -47,11 +49,8 @@ for url in faas_url:
         dl_station = decoded_line + station_name #added station name as column
         current_file.append(dl_station.split()) #split data by blankspace
     combined_files.append(current_file) 
-    #clean up header
-    #del file_as_is[:2] #removed first two rows (headers)
-#combined_files.insert(0,header) #adding in clean header
-#     #save as csv
+#save as csv
 file_with_header = flatten(combined_files)
 file_with_header.insert(0,header) #adding in clean header
-np.savetxt("all_stations_v2.csv",file_with_header,delimiter=",",fmt='% s')
+np.savetxt("all_stations_v3.csv",file_with_header,delimiter=",",fmt='% s')
 
