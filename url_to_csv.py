97caf665dab2ff9227781095ef1ea0d8aad61418
Maria Briceno-Rojas <mafer09@users.noreply.github.com>
2022-01-29 09:38:17 -0600

    updates
    picks up from 3 urls and adds in station name as a column

--- url_to_csv.py
+++ url_to_csv.py
@@ -1,19 +1,39 @@
 import urllib.request
+import re
 import numpy as np
 
-site_url = "https://www.ndbc.noaa.gov/data/realtime2/KIKT.txt"
-file = urllib.request.urlopen(site_url)
+'''
+    input: list of list
+    output: flattened list
+    ref: https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-a-list-of-lists
+'''
+def flatten(data):
+    return [item for sublist in data for item in sublist]
 
-file_as_is = []
-for line in file:
-    decoded_line = line.decode("utf-8")
-    #strip_content = decoded_line.split()
-    #if (decoded_line )
-    #print(decoded_line)
-    file_as_is.append(decoded_line.split())
+source_urls = ["https://www.ndbc.noaa.gov/data/realtime2/KIKT.txt","https://www.ndbc.noaa.gov/data/realtime2/KBQX.txt","https://www.ndbc.noaa.gov/data/realtime2/KMIS.txt"]
+header = ["year", "month", "day", "hour", "minute", "WDIR_wind_direction","WSPD_wind_speed","GST_gust_speed","WVHT_wave_height","DPD_dominant_wave_period","APD_average_wave_period","MWD_wave_degree","PRES_sea_level_pressure","ATMP_air_temperature","WTMP_sea_surface_temperature","DEWP_dewpoint_temperature","VIS_station_visibility","PTDY_pressure_tendency","TIDE_water_level","station_name"]
 
-del file_as_is[:2] #removed first two rows (headers)
-header = ["year", "month", "day", "hour", "minute", "WDIR_wind_direction","WSPD_wind_speed","GST_gust_speed","WVHT_wave_height","DPD_dominant_wave_period","APD_average_wave_period","MWD_wave_degree","PRES_sea_level_pressure","ATMP_air_temperature","WTMP_sea_surface_temperature","DEWP_dewpoint_temperature","VIS_station_visibility","PTDY_pressure_tendency","TIDE_water_level"]
-file_as_is.insert(0,header) #adding in clean header
+combined_files = []
+for url in source_urls:
+    #retrieve station name from url
+    pattern = '2/(.*?)\.'
+    station_name = re.search(pattern, url).group(1)
+    if station_name == 'KBQX': station_name = 'KAPT'
+    #get data
+    file = urllib.request.urlopen(url)
+    current_file = []
+    for line in file:
+        decoded_line = line.decode("utf-8")
+        if decoded_line.startswith('#'): #skip header
+            continue
+        dl_station = decoded_line + station_name #added station name as column
+        current_file.append(dl_station.split()) #split data by blankspace
+    combined_files.append(current_file) 
+    #clean up header
+    #del file_as_is[:2] #removed first two rows (headers)
+#combined_files.insert(0,header) #adding in clean header
+#     #save as csv
+file_with_header = flatten(combined_files)
+file_with_header.insert(0,header) #adding in clean header
+np.savetxt("all_stations.csv",file_with_header,delimiter=",",fmt='% s')
 
-np.savetxt("KIKT.csv",file_as_is,delimiter=",",fmt='% s') #need to make dynamic
